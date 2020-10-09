e752b31f70010e05c056ae008b8db90a561c4d35
akapikumin <akapikumin100piki@gmail.com>
2020-10-09 15:54:35 +0900

    Initial Commit

---
+++ new note/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  new note
+//
+//  Created by ガンジス on 2020/10/09.
+//  Copyright © 2020 ガンジス. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
